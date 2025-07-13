2c93f5865c68c3a65efa93f1526538b65a21d4cd
Pranav <pranavkp.me@outlook.com>
2025-07-13 09:41:55 +0530

    update homepage with new title and fancy button component; add typography styles for headings and labels

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,8 +1,12 @@
-import * as Button from '@reloop/ui/components/button';
+import * as Button from '@reloop/ui/components/fancy-button';
 
 export default function Home() {
   return (
     <div>
+      <h1 className="title-h1">
+        Welcome to Reloop UI!
+        <span className="text-title-h2">A React component library</span>
+      </h1>
       <Button.Root variant="neutral">Give a star</Button.Root>
     </div>
   );
