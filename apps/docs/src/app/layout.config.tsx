9a0a5d7fd61fa2fcb0e12acc77f0ad96d596aee9
Pranav <pranavkp.me@outlook.com>
2025-08-03 22:56:34 +0530

    fix the docs content

--- apps/docs/src/app/layout.config.tsx
+++ apps/docs/src/app/layout.config.tsx
@@ -1,4 +1,3 @@
-import { Icon } from "@reloop/ui/components/icon";
 import { Logo } from "@reloop/ui/components/logo";
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
@@ -15,45 +14,4 @@ export const baseOptions: BaseLayoutProps = {
 		title: <Logo className="h-10 w-10" />,
 	},
 	// see https://fumadocs.dev/docs/ui/navigation/links
-	links: [
-		// {
-		// 	icon: <Icon name="terminal" />,
-		// 	text: "Project Structure",
-		// 	url: "/project-structure",
-		// 	// secondary items will be displayed differently on navbar
-		// 	secondary: false,
-		// },
-		// {
-		// 	icon: <Icon name="terminal" />,
-		// 	text: "How to Contribute",
-		// 	url: "/docs/how-to-contribute",
-		// 	// secondary items will be displayed differently on navbar
-		// 	secondary: false,
-		// },
-		// {
-		// 	icon: <Icon name="terminal" />,
-		// 	text: "test1",
-		// 	url: "/docs/test1",
-		// 	// secondary items will be displayed differently on navbar
-		// 	secondary: false,
-		// },
-		// {
-		// 	icon: <Icon name="terminal" />,
-		// 	text: "Local Setup",
-		// 	url: "/local-setup",
-		// 	// secondary items will be displayed differently on navbar
-		// 	secondary: false,
-		// },
-		{
-			icon: <Icon name="terminal" />,
-			text: "How to Contribute",
-			url: "/how-to-contribute",
-		},
-
-		{
-			icon: <Icon name="terminal" />,
-			text: "How to Self Host",
-			url: "/how-to-self-host",
-		},
-	],
 };
