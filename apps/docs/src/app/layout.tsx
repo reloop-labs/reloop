819d5039c033c881e41040b901fa4f24162a7112
Pranav <pranavkp.me@outlook.com>
2025-08-04 11:07:24 +0530

    updated fonts

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -1,34 +1,14 @@
 import "@/app/global.css";
 import { IconsSprite } from "@reloop/ui/components/icon";
 import { RootProvider } from "fumadocs-ui/provider";
-import {
-	Geist_Mono as createMono,
-	Geist as createSans,
-} from "next/font/google";
 import type { ReactNode } from "react";
 import { cn } from "@/lib/cn";
-
-const sans = createSans({
-	subsets: ["latin"],
-	variable: "--font-sans",
-	weight: "variable",
-});
-
-const mono = createMono({
-	subsets: ["latin"],
-	variable: "--font-mono",
-	weight: "variable",
-});
 
 export default function Layout({ children }: { children: ReactNode }) {
 	return (
 		<html
 			lang="en"
-			className={cn(
-				"touch-manipulation scroll-smooth font-sans antialiased",
-				sans.variable,
-				mono.variable,
-			)}
+			className={cn("touch-manipulation scroll-smooth antialiased")}
 			suppressHydrationWarning
 		>
 			<body className="flex min-h-screen flex-col">
