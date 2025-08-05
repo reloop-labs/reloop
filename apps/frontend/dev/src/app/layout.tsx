fb5dbac565ffa14a873cbd0a3188be069e5714c6
Pranav <pranavkp.me@outlook.com>
2025-08-05 21:10:10 +0530

    added dev docs

---
+++ apps/frontend/dev/src/app/layout.tsx
@@ -0,0 +1,20 @@
+import "@/app/global.css";
+import { IconsSprite } from "@reloop/ui/components/icon";
+import { RootProvider } from "fumadocs-ui/provider";
+import type { ReactNode } from "react";
+import { cn } from "@/lib/cn";
+
+export default function Layout({ children }: { children: ReactNode }) {
+	return (
+		<html
+			lang="en"
+			className={cn("touch-manipulation scroll-smooth antialiased")}
+			suppressHydrationWarning
+		>
+			<body className="flex min-h-screen flex-col">
+				<RootProvider>{children}</RootProvider>
+				<IconsSprite />
+			</body>
+		</html>
+	);
+}
