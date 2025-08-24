9d15d2369f63eee401ef7657d00685702e7c1d1c
Pranav <pranavkp.me@outlook.com>
2025-08-24 10:59:34 +0530

    Update Next.js configuration: add asset prefix for production and enable trailing slashes.

--- apps/frontend/dev/next.config.mjs
+++ apps/frontend/dev/next.config.mjs
@@ -1,10 +1,13 @@
 import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
+const isProd = process.env.NODE_ENV === "production";
 
 /** @type {import('next').NextConfig} */
 const config = {
 	reactStrictMode: true,
+	assetPrefix: isProd ? "/dev" : undefined,
+	trailingSlash: true,
 };
 
 export default withMDX(config);
