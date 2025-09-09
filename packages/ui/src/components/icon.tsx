b55008699b55171a2bea2bd02bb52800bc4734bb
Pranav <pranavkp.me@outlook.com>
2025-09-10 01:20:44 +0530

    Update UI package to integrate Tailwind CSS; replace global styles import, add PostCSS configuration, and adjust package dependencies. Refactor icon component for consistent attribute naming.

--- packages/ui/src/components/icon.tsx
+++ packages/ui/src/components/icon.tsx
@@ -325,9 +325,9 @@ export const IconsSprite = () => (
 				<path
 					d="M19 19L5 5M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
 					stroke="currentColor"
-					stroke-width="1.5"
-					stroke-linecap="round"
-					stroke-linejoin="round"
+					strokeWidth="1.5"
+					strokeLinecap="round"
+					strokeLinejoin="round"
 				/>
 			</symbol>
 			<symbol id="headphone" viewBox="0 0 24 24">
