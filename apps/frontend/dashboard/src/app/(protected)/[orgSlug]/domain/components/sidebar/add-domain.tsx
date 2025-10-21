25028b27969706da54ec56ab31a820fa61871d93
Pranav <pranavkp.me@outlook.com>
2025-10-22 01:43:25 +0530

    feat: enhance AddDomainSidebar component with improved layout and validation, including a globe icon and refined form structure for better user experience

--- apps/frontend/dashboard/src/app/(protected)/[orgSlug]/domain/components/sidebar/add-domain.tsx
+++ apps/frontend/dashboard/src/app/(protected)/[orgSlug]/domain/components/sidebar/add-domain.tsx
@@ -12,6 +12,7 @@ import type { Resolver } from "react-hook-form";
 import { useForm } from "react-hook-form";
 import { useSWRConfig } from "swr";
 import * as v from "valibot";
+import { Globe } from "../../globe";
 
 const domainSchema = v.object({
 	domain: v.pipe(
@@ -67,22 +68,65 @@ export const AddDomainSidebar = () => {
 
 	return (
 		<div className="mx-auto max-w-3xl">
-			<div className="my-10 flex items-center gap-3">
-				<div>
+			<div className="my-10 flex gap-3">
+				<Icon name="globe" className="ml-4 h-8 w-8 rounded-full" />
+				<div className="ml-4">
 					<h1 className="font-medium text-title-h4 leading-8">Add Domain</h1>
 					<p className="text-paragraph-sm text-text-sub-600">
 						Add a new domain and start sending emails from your domain
 					</p>
 				</div>
 			</div>
-			<div>
-				<div>
-					<p className="font-medium text-title-h5">Domain</p>
-					<p className="text-paragraph-sm text-text-sub-600">
-						Add a new domain send emails from your domain
-					</p>
-					<form onSubmit={handleSubmit(onSubmit)}>
-						<div className="rounded-2xl border border-stroke-soft-200 p-4">
+			<div className="relative my-10 ml-8 border-stroke-soft-200 border-l py-10">
+				<div className="relative flex flex-col pl-10">
+					<div className="-left-3.5 absolute top-1 rounded-full bg-bg-white-0 p-2">
+						<div className="h-3 w-3 rounded-full border-2 bg-bg-white-0" />
+					</div>
+					<div className="flex gap-10">
+						<div>
+							<p className="font-medium text-title-h5">Domain</p>
+							<p className="text-paragraph-sm text-text-sub-600">
+								Add a new domain send emails from your domain
+							</p>
+							<form onSubmit={handleSubmit(onSubmit)} className="w-96">
+								<div className="mt-5">
+									<Label.Root htmlFor="domain">
+										Domain
+										<Label.Asterisk />
+									</Label.Root>
+									<Input.Root hasError={!!formState?.errors?.domain?.message}>
+										<Input.Affix>https://</Input.Affix>
+										<Input.Wrapper>
+											<Input.Input
+												id="domain"
+												placeholder="www.example.com"
+												{...register("domain")}
+												disabled={status === "loading"}
+											/>
+										</Input.Wrapper>
+									</Input.Root>
+									{formState.errors.domain && (
+										<p className="mt-1 text-error-base text-paragraph-sm">
+											{formState.errors.domain.message}
+										</p>
+									)}
+								</div>
+								<div className="flex w-96 justify-end">
+									<Button.Root
+										type="submit"
+										className="mt-5"
+										variant="neutral"
+										disabled={status === "loading" || !formState.isValid}
+									>
+										{status === "loading" && (
+											<Spinner color="var(--text-strong-950)" />
+										)}
+										{status === "loading" ? "Adding Domain..." : "Add Domain"}
+									</Button.Root>
+								</div>
+							</form>
+						</div>
+						<div className="mt-24 h-fit rounded-2xl border border-stroke-soft-200 p-4">
 							<div className="flex items-center gap-2 uppercase">
 								<Icon name="bulb" className="h-4 w-4" />
 								<p>Recommendations</p>
@@ -99,45 +143,17 @@ export const AddDomainSidebar = () => {
 								</ul>
 							</div>
 						</div>
-						<div className="mt-5">
-							<Label.Root htmlFor="domain">
-								Domain
-								<Label.Asterisk />
-							</Label.Root>
-							<Input.Root hasError={!!formState?.errors?.domain?.message}>
-								<Input.Affix>https://</Input.Affix>
-								<Input.Wrapper>
-									<Input.Input
-										id="domain"
-										placeholder="www.example.com"
-										{...register("domain")}
-										disabled={status === "loading"}
-									/>
-								</Input.Wrapper>
-							</Input.Root>
-							{formState.errors.domain && (
-								<p className="mt-1 text-error-base text-paragraph-sm">
-									{formState.errors.domain.message}
-								</p>
-							)}
-						</div>
-						<div className="flex w-96 justify-end">
-							<Button.Root
-								type="submit"
-								className="mt-5"
-								variant="neutral"
-								disabled={status === "loading" || !formState.isValid}
-							>
-								{status === "loading" && (
-									<Spinner color="var(--text-strong-950)" />
-								)}
-								{status === "loading" ? "Adding Domain..." : "Add Domain"}
-							</Button.Root>
-						</div>
-					</form>
+					</div>
+				</div>
+				<div className="relative mt-10 pl-10">
+					<div className="-left-3.5 absolute top-1 rounded-full bg-bg-white-0 p-2">
+						<div className="h-3 w-3 rounded-full border-2 border-stroke-soft-200 bg-bg-white-0" />
+					</div>
+					<p className="font-medium text-text-sub-600 text-title-h5">
+						DNS Records
+					</p>
 				</div>
 			</div>
-			<p className="font-medium text-text-sub-600 text-title-h5">DNS Records</p>
 		</div>
 	);
 };
