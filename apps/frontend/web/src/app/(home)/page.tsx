a913811ab294e2900be5044b8bc85ebfe4f33dec
Pranav <pranavkp.me@outlook.com>
2025-11-04 01:35:16 +0530

    feat: refactor Home page by introducing Hero and Security components for enhanced content structure

--- apps/frontend/web/src/app/(home)/page.tsx
+++ apps/frontend/web/src/app/(home)/page.tsx
@@ -1,38 +1,12 @@
-import * as Button from "@reloop/ui/button";
-import Link from "next/link";
+import Hero from "./components/hero";
+import Security from "./components/security";
 
 export default function Home() {
 	return (
-		<div className="mx-auto max-w-7xl border-stroke-soft-100 border-r border-l pt-20">
-			<h1 className="title-h2 mx-auto max-w-xl text-center font-semibold">
-				Email for Developers & Marketing teams
-			</h1>
-			<h2 className="mx-auto mt-6 max-w-lg text-center text-lg text-text-sub-600 leading-8">
-				Reloop provides secure, reliable, and scalable email infrastructure with
-				99.9% inbox placement never spam.{" "}
-			</h2>
-
-			<div className="mt-10 flex items-center justify-center gap-4">
-				<Link
-					href="/contact"
-					className={Button.buttonVariants({
-						variant: "neutral",
-					}).root({})}
-				>
-					Get Early Access
-				</Link>
-				<a
-					target="_blank"
-					href="https://github.com/reloop-labs/reloop"
-					className={Button.buttonVariants({
-						mode: "stroke",
-						variant: "neutral",
-					}).root({})}
-					rel="noopener"
-				>
-					View it on GitHub
-				</a>
-			</div>
+		<div>
+			<Hero />
+			<Security />
+			<p>sd</p>
 		</div>
 	);
 }
