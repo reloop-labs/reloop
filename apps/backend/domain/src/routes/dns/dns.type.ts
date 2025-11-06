e8e6555c263eb2c316642f609591f2ef91e056b0
rudra-2 <rmp20026@gmail.com>
2025-11-06 22:34:03 +0530

    postfix docker compose

--- apps/backend/domain/src/routes/dns/dns.type.ts
+++ apps/backend/domain/src/routes/dns/dns.type.ts
@@ -1,4 +1,4 @@
-import type { DNSModel } from "@reloop/domain/routes/dns/dns.model";
+	import type { DNSModel } from "@reloop/domain/routes/dns/dns.model";
 
 export namespace DNSTypes {
 	export type DNSRecordResponse = typeof DNSModel.dnsRecordResponse.static;
@@ -46,3 +46,4 @@ export namespace DNSTypes {
 		status: DNSRecordStatus;
 	}
 }
+	
