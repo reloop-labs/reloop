60a4fcf02c571297793da962c6c4334964dd58dd
twinkalp10 <twinkal.work@outlook.com>
2025-11-15 16:45:30 +0530

    refactor: remove unused DNS record generation handler to streamline controller exports

--- apps/backend/domain/src/routes/dns/controllers/index.ts
+++ apps/backend/domain/src/routes/dns/controllers/index.ts
@@ -1,5 +1,4 @@
 export { deleteDNSRecordsHandler } from "@be/domain/routes/dns/controllers/delete-dns-records";
-export { generateDNSRecordsHandler } from "@be/domain/routes/dns/controllers/generate-dns-records";
 export { getDKIMKeysHandler } from "@be/domain/routes/dns/controllers/get-dkim-keys";
 export { getDNSRecordsHandler } from "@be/domain/routes/dns/controllers/get-dns-records";
 export { verifyDNSRecordHandler } from "@be/domain/routes/dns/controllers/verify-dns-record";
