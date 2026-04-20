4bd1459b3662763f52a23c086fe18c1df237f42f
Pranav <pranavkp.me@outlook.com>
2026-04-20 19:35:02 +0530

    feat: implement and integrate UseCaseGrid component on the homepage

--- apps/frontend/web/src/app/(home)/page.tsx
+++ apps/frontend/web/src/app/(home)/page.tsx
@@ -1,25 +1,18 @@
-import Company from "./components/company";
 import CTA from "./components/cta";
 import Faq from "./components/faq";
 import Hero from "./components/hero";
 import HowItWorks from "./components/how-it-works";
 import OpenSource from "./components/open-source";
-import { Scale } from "./components/scale";
-import Sdk from "./components/sdk";
-import Security from "./components/security";
-import UseCase from "./components/use-case";
+import UseCaseGrid from "./components/use-case-grid";
+
 export default function Home() {
 	return (
 		<div>
 			<Hero />
-			<Sdk />
-			<UseCase />
 			<HowItWorks />
-			<Scale />
-			<Security />
-			<Company />
 			<CTA />
 			<OpenSource />
+			<UseCaseGrid />
 			<Faq />
 		</div>
 	);
