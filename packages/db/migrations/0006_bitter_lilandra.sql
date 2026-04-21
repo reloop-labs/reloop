03fba85187d23af03bdf212916f5e1d346aa3860
Pranav <pranavkp.me@outlook.com>
2026-04-21 10:51:31 +0530

    chore: remove all database migration files and metadata snapshots

--- packages/db/migrations/0006_bitter_lilandra.sql
+++
@@ -1,5 +0,0 @@
-CREATE TYPE "public"."tls_mode" AS ENUM('opportunistic', 'enforced');--> statement-breakpoint
-ALTER TABLE "domain" ADD COLUMN "custom_return_path" varchar(255) DEFAULT 'send' NOT NULL;--> statement-breakpoint
-ALTER TABLE "domain" ADD COLUMN "click_tracking" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "domain" ADD COLUMN "open_tracking" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "domain" ADD COLUMN "tls" "tls_mode" DEFAULT 'opportunistic' NOT NULL;
