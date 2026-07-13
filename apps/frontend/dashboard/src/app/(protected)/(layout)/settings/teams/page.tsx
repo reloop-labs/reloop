e5676294acee23fe2561e96091ed7c5c6a3a3759
Pranav <pranavkp.me@outlook.com>
2026-07-13 21:37:50 +0530

    style: add rounded-xl border radius to invite members button

--- apps/frontend/dashboard/src/app/(protected)/(layout)/settings/teams/page.tsx
+++ apps/frontend/dashboard/src/app/(protected)/(layout)/settings/teams/page.tsx
@@ -76,6 +76,7 @@ const Team = () => {
 						variant="neutral"
 						size="xsmall"
 						onClick={() => setModal("invite")}
+						className="rounded-xl"
 					>
 						<Icon name="user-plus" className="h-4 w-4" />
 						<span>Invite members</span>
