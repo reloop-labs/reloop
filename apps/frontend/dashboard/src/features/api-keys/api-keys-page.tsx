f5b39a7a7cb81855339058e018a38bb157fa502e
twinkalp10 <twinkal.work@outlook.com>
2026-08-01 14:32:14 +0530

    feat: replace CommonUseCasesSidebar with ApiKeysResourcesBanner in ApiKeysPage for improved resource visibility

--- apps/frontend/dashboard/src/features/api-keys/api-keys-page.tsx
+++ apps/frontend/dashboard/src/features/api-keys/api-keys-page.tsx
@@ -1,19 +1,13 @@
-import { CommonUseCasesSidebar } from "./common-use-cases-sidebar";
+import { ApiKeysResourcesBanner } from "./api-keys-resources-banner";
 import { ApiKeyList } from "./list/api-key-list";
 import { ApiKeyListHeader } from "./list/api-key-list-header";
 
 export function ApiKeysPage() {
 	return (
-		<div className="mx-auto max-w-6xl space-y-6 p-6 lg:p-8">
+		<div className="mx-auto max-w-7xl space-y-6 p-6 lg:p-8">
 			<ApiKeyListHeader />
-			<div className="grid grid-cols-1 gap-8 lg:grid-cols-12">
-				<div className="lg:col-span-8 xl:col-span-8">
-					<ApiKeyList />
-				</div>
-				<div className="lg:col-span-4 xl:col-span-4">
-					<CommonUseCasesSidebar />
-				</div>
-			</div>
+			<ApiKeysResourcesBanner />
+			<ApiKeyList />
 		</div>
 	);
 }
