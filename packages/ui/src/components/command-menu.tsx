2465adfc87939f5c2cf6268d661d7c50a739f0e7
Pranav <pranavkp.me@outlook.com>
2026-08-02 11:44:59 +0530

    style: add vertical padding to command menu list container

--- packages/ui/src/components/command-menu.tsx
+++ packages/ui/src/components/command-menu.tsx
@@ -80,7 +80,7 @@ const CommandList = React.forwardRef<
 		<Command.List
 			ref={forwardedRef}
 			className={cn(
-				"flex max-h-min min-h-0 flex-1 flex-col overflow-auto [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden [&_[cmdk-list-sizer]]:[-ms-overflow-style:none] [&_[cmdk-list-sizer]]:[scrollbar-width:none] [&_[cmdk-list-sizer]]:[&::-webkit-scrollbar]:hidden",
+				"flex max-h-min min-h-0 flex-1 flex-col overflow-auto pt-1.5 pb-2 [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden [&_[cmdk-list-sizer]]:[-ms-overflow-style:none] [&_[cmdk-list-sizer]]:[scrollbar-width:none] [&_[cmdk-list-sizer]]:[&::-webkit-scrollbar]:hidden",
 				className,
 			)}
 			{...rest}
